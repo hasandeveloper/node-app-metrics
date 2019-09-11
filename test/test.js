e975b8ad094eb97abdc8f31ec448ab88d90d3d87
Aniket Rao <aniket.rao@go-jek.com>
2019-09-11 20:15:22 +0530

    [Aniket] - removed unwanted code

--- test/test.js
+++ test/test.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('App', function() {
-  it('has the default page', function(done) {
+  it('responds pong', function(done) {
     request(app)
-      .get('/')
-      .expect(/Welcome to Express/, done);
+      .get('/ping')
+      .expect({message: 'PONG'}, done);
   });
 }); 
