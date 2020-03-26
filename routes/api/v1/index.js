4c043c5c44101d3655f3256a45d71cd135b94099
hasan <hasan.ali@kloctechnologies.co.in>
2020-03-26 11:25:35 +0530

    chore: npm installed

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const statsD = require('../../../middlewares/statsd')
-// const appstatsd = require('appmetrics-statsd').StatsD({prefix: 'hello'});
+const appstatsd = require('appmetrics-statsd').StatsD({
+  prefix: 'example-app'
+});
 
-var SDC = require('statsd-client')
-const sdc = new SDC({prefix: 'statsd.example.'})
 
 /* GET home page. */
-router.get('/hello', statsD('hello'), function(req, res, next) {
-  sdc.increment('hello.200');
+router.get('/hello', function(req, res, next) {
   res.status(200).send({
     message: 'Hello World!'
   });
